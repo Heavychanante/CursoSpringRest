fd85497d872addfe6c490d0e97c5a1ed4276e49e
hperezblasco <hperezblasco@gmail.com>
2016-03-16 20:18:45 +0100

    Commit inicial

---
+++ src/main/java/com/curso/springrest/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.curso.springrest;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CursoSpringRestApplication.class);
+	}
+
+}
